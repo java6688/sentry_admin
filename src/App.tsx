163c3384075cde74721c28f2002eb48010f7f784
vilan_微澜 <1253490794@qq.com>
2026-01-09 00:15:50 +0800

    style: trae

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { AliveScope, KeepAlive } from "react-activation";
 import Login from "./pages/Login/index";
 import Home from "./pages/Home/index";
 import Dashboard from "./pages/Dashboard/index";
@@ -9,15 +8,13 @@ import "./App.css";
 function App() {
   return (
     <BrowserRouter>
-      <AliveScope>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/dashboard" element={<KeepAlive><Dashboard /></KeepAlive>} />
-          <Route path="/error/:id" element={<ErrorDetail />} />
-          <Route path="/" element={<Navigate to="/login" replace />} />
-        </Routes>
-      </AliveScope>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/error/:id" element={<ErrorDetail />} />
+        <Route path="/" element={<Navigate to="/login" replace />} />
+      </Routes>
     </BrowserRouter>
   );
 }
