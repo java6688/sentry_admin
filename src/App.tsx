cba371cfd2e5194c822eb98391657a2a7054f60d
vilan_微澜 <1253490794@qq.com>
2026-01-19 15:38:17 +0800

    feat: 增加信息面板的路由缓存

--- src/App.tsx
+++ src/App.tsx
@@ -13,39 +13,40 @@ import { ProtectedRoute } from "./components/ProtectedRoute";
 import RBACRoute from "./components/RBAC/RBACRoute";
 import { UserProvider } from "./contexts/UserContext";
 import "./App.css";
+import { AliveScope, KeepAlive } from "react-activation";
 
 function App() {
   return (
     <BrowserRouter>
-      <UserProvider>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          {/* 受保护路由 */}
-          <Route element={<ProtectedRoute />}>
-            <Route path="/home" element={<MainLayout><Home /></MainLayout>} />
-            <Route element={<RBACRoute requiredPerms={["error:read"]} />}>
-              <Route path="/dashboard" element={<MainLayout><Dashboard /></MainLayout>} />
-              <Route path="/error/:id" element={<MainLayout><ErrorDetail /></MainLayout>} />
+      <AliveScope>
+        <UserProvider>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route element={<ProtectedRoute />}>
+              <Route path="/home" element={<MainLayout><Home /></MainLayout>} />
+              <Route element={<RBACRoute requiredPerms={["error:read"]} />}>
+                <Route path="/dashboard" element={<MainLayout><KeepAlive name="dashboard"><Dashboard /></KeepAlive></MainLayout>} />
+                <Route path="/error/:id" element={<MainLayout><ErrorDetail /></MainLayout>} />
+              </Route>
+              <Route element={<RBACRoute requiredPerms={["rbac:role:read"]} />}>
+                <Route path="/rbac/roles" element={<MainLayout><Roles /></MainLayout>} />
+              </Route>
+              <Route element={<RBACRoute requiredPerms={["rbac:perm:read"]} />}>
+                <Route path="/rbac/permissions" element={<MainLayout><Permissions /></MainLayout>} />
+              </Route>
+              <Route element={<RBACRoute requiredPerms={["user:read"]} />}>
+                <Route path="/rbac/user-roles" element={<MainLayout><UserRoles /></MainLayout>} />
+              </Route>
+              <Route element={<RBACRoute requiredPerms={["rbac:role:setPermissions"]} />}>
+                <Route path="/rbac/assign-permissions" element={<MainLayout><AssignRolePermissions /></MainLayout>} />
+              </Route>
+              <Route path="/profile" element={<MainLayout><Profile /></MainLayout>} />
+              <Route path="/rbac" element={<Navigate to="/rbac/roles" replace />} />
             </Route>
-            <Route element={<RBACRoute requiredPerms={["rbac:role:read"]} />}>
-              <Route path="/rbac/roles" element={<MainLayout><Roles /></MainLayout>} />
-            </Route>
-            <Route element={<RBACRoute requiredPerms={["rbac:perm:read"]} />}>
-              <Route path="/rbac/permissions" element={<MainLayout><Permissions /></MainLayout>} />
-            </Route>
-            <Route element={<RBACRoute requiredPerms={["user:read"]} />}>
-              <Route path="/rbac/user-roles" element={<MainLayout><UserRoles /></MainLayout>} />
-            </Route>
-            <Route element={<RBACRoute requiredPerms={["rbac:role:setPermissions"]} />}>
-              <Route path="/rbac/assign-permissions" element={<MainLayout><AssignRolePermissions /></MainLayout>} />
-            </Route>
-            <Route path="/profile" element={<MainLayout><Profile /></MainLayout>} />
-            <Route path="/rbac" element={<Navigate to="/rbac/roles" replace />} />
-          </Route>
-          {/* 根路径重定向到登录页 */}
-          <Route path="/" element={<Navigate to="/login" replace />} />
-        </Routes>
-      </UserProvider>
+            <Route path="/" element={<Navigate to="/login" replace />} />
+          </Routes>
+        </UserProvider>
+      </AliveScope>
     </BrowserRouter>
   );
 }
